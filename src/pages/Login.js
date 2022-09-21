d4ff9a3bda541633780d8a4f4b79b6d8596b7f13
jessezorzela <jesse.zorzela4242@gmail.com>
2022-09-20 22:45:54 -0300

    t

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,172 +1,125 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
-import api from '../services/api'
-import logo from '../assets/logo.png';
-import EsqueciSenha from "./EsqueciSenha"
+import React, {useState, useEffect, useRef} from "react";
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Modal, Image } from "react-native";
+import { Camera } from 'expo-camera';
+import { FontAwesome} from '@expo/vector-icons';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 
-export default function Login( {navigation} ) {
-  const[email, setEmail] = useState('cedrick07@example.net');
-  const[password, setPassword] = useState('password');
+export default function Login({navigation}) {
+  const camRef = useRef
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHaspermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
+  
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestPermissionsAsync();
+      setHaspermission(status === 'granted');
+    })();
 
-  useState(() => {
-    console.log(localStorage.getItem('token'));
-    if(localStorage.getItem('token') !== null){
-      navigation.navigate('Index')
+    (async () => {
+      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      setHaspermission(status === 'granted');
+    })();
+
+  }, []);
+
+    if(hasPermission === null){
+      return <View/>;
     }
-  },[localStorage]);
-  function singin(){
-    api.post('login', {
-     email: email,
-     password : password
-    }).then( async response => {
-      console.log(response.data);
-      const token  = response.data.acess_token;
-      localStorage.setItem('token', token);
-      //api.headers.Authorization = `Bearer ${token}`;
-      navigation.navigate('Index');
-    }).catch(err => {
-      alert ('Usuario ou senha incorretos');
-    });
-  }
-  
-  function cadastro(){
-    console.log(email)
-          navigation.navigate('Cadastro')
-  }
-  function esquecisenha(){
-    console.log(email)
-          navigation.navigate('EsqueciSenha')
-  }
-  
-  return (
-    <View style={styles.container}>
-      <LinearGradient
-        colors={['#0E5CBB', '#2E75E7']}
-        style={styles.background}>
-        <Image source={logo} style={styles.logo} />
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>E-mail</Text>
-          <TextInput style={styles.input}
-            keyboardType="email-address"
-            placeholder="E-mail"
-            placeholderTextColor="#999"
-            autoCapitalize='none'
-            autoCorrect={false}
-            name='email'
-            onChangeText={setEmail}
-            value={email}
-          ></TextInput>
-        </View>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Senha</Text>
-          <TextInput style={styles.input}
-            secureTextEntry={true}
-            keyboardType="visible-password"
-            placeholder="Digite sua Senha:"
-            placeholderTextColor="#999"
-            autoCapitalize='none'
-            autoCorrect={false}
-            name='password'
-            onChangeText={setPassword}
-            value={password}
-          ></TextInput>
-        </View>
 
-        <TouchableOpacity 
-        style={[styles.button, styles.backgroundButton]}
-        onPress={singin}
-        >
-          <Text style={styles.text}>Logar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={cadastro}
-         >
-          <Text style={styles.text}>Faça seu cadastro!</Text>
-        </TouchableOpacity >
-        <TouchableOpacity style={styles.esqueci}>
-          <Text style={styles.text}
-          onPress={esquecisenha}
-          >Esqueci minha senha</Text>
-          <Text>{email}</Text>
+    if(hasPermission === false){
+      return <Text> Acesso Negado! </Text>;
+    }
+
+  async function takePicture() {
+    if(camRef){
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
+      console.log(data);
+    }
+  }
+
+  async function savePicture() {
+    const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
+    .then(()=> {
+      alert('Salvo com sucesso!')
+    })
+    .catch(error => {
+      console.log('err', error);
+    })
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={{ flex: 1}}
+        type={type}
+        ref={camRef}
+      >
+        <View style={{flex:1, backgroundColor: 'transparent', flexDirection: 'row'}}>
+          <TouchableOpacity
+          style={{
+            position: 'absolute',
+            bottom: 20,
+            left: 20,
+            }}
+            onPress={ () => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+                );
+            }}
+            >
+            <View style={{ fontSize: 20, marginBottom: 13, color: '#fff'}}>Trocar</View>
           </TouchableOpacity>
-      </LinearGradient>
-    </View>
-  );
+        </View>
+      </Camera>
+      <TouchableOpacity style={styles.button} onPress={ takePicture }>
+            <FontAwesome name="csmera" size={23} color="#fff"/>
+      </TouchableOpacity>
+
+      { capturedPhoto &&
+        <Modal
+        animationType="slide"
+        transparent={false}
+        visible={open}>
+          <View style={{flex: 1, justifyContent: 'center', alignItems:'center', margin: 20}}>
+            <View style={{ margin: 10, flexDirection: 'row'}}>
+              <TouchableOpacity style={{ margin: 10}} onPress={ () => setOpen(false)}>
+                <FontAwesome name="window-close" size={50} color="#FF0000"/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ margin: 10}} onPress={ savePicture}>
+                <FontAwesome name="upload" size={50} color="#121212"/>
+              </TouchableOpacity>
+            </View>
+            <Image
+              style={{ width: '100%', height: 500, borderRadius: 15}}
+              source={{ uri: capturedPhoto}}/>
+          </View>
+        </Modal>
+      }
+
+    </SafeAreaView>
+  )
 }
+  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  background: {
-    flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
   },
-  logo: {
-    width: 150,
-    height: 150,
-    marginBottom: 20,
-  },
-  formGroup: {
-    width: '70%',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-  },
-  label: {
-    color: '#fff',
-    fontSize: 10,
-    backgroundColor: "#4DCFE0",
-    paddingHorizontal: 15,
-    paddingVertical: 0,
-    lineHeight: 20,
-    borderRadius: 5,
-    zIndex:10,
-    marginLeft: 10,
-  },
-  input: {
-    marginTop:-10,
-    backgroundColor: "#fff",
-    borderWidth: 1,
-    borderColor: '#DDD',
-    paddingHorizontal: 20,
-    marginBottom: 10,
-    fontSize: 14,
-    height: 45,
-    width: '100%',
-    borderRadius: 5
-  }, 
   button: {
     justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: '#DDD',
-    paddingHorizontal: 20,
-    marginBottom: 10,
-    fontSize: 14,
-    height: 45,
-    width: '70%',
-    textAlign: 'center',
     alignItems: 'center',
-    borderRadius: 5
-  },
-  backgroundButton: {
-    backgroundColor: "#4DCFE0",
-    borderColor : 'transparent',
-    marginTop: 30,
-  },
-  text: {
-    backgroundColor: 'transparent',
-    color: '#fff',
-  },
-  esqueci: {
-    position: 'absolute',
-    bottom: 10,
+    backgroundColor: '#121212',
+    margin: 20,
+    borderRadius: 10,
+    height: 50,
   }
 });
